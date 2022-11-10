076b0d89e68d61adb139be0fa9818ff99e42d3a8
Mwalim19 <111836932+Mwalim19@users.noreply.github.com>
2022-11-11 00:47:17 +0800

    code update

--- solution.R
+++ solution.R
@@ -1,3 +1,7 @@
+###References
+##https://en.wikipedia.org/wiki/Bertrand_paradox_(probability)
+##https://hpaulkeeler.com/the-bertrand-paradox/
+
 library(tidyverse)
 library(ggforce)
 theme_set(theme_void()) 
@@ -6,7 +10,8 @@ x0=0; y0=0; #center of disk
 r=1; #radius
 numbLines=100; #number of lines
 
-# Method A..........................................
+# Method A..........................................................................
+
 thetaA1=2*pi*runif(numbLines); #choose angular component uniformly
 thetaA2=2*pi*runif(numbLines); #choose angular component uniformly
 
@@ -19,6 +24,7 @@ eqtri_df <- tibble(
 )
 
 # Coordinates of random chord
+
 rdmchr_A <- tibble(
   x    = x0+r*cos(thetaA1),
   y    = y0+r*sin(thetaA1),
@@ -26,28 +32,38 @@ rdmchr_A <- tibble(
   yend = y0+r*sin(thetaA2)
 )
 # calculate probability that the chord is longer than a side of the triangle
+xA    = x0+r*cos(thetaA1)
+yA    = y0+r*sin(thetaA1)
+xAend = x0+r*cos(thetaA2)
+yAend = y0+r*sin(thetaA2)
+#length of triangle side
+lengthSide<-r*sqrt(3) 
+#lengths of the chord
+lengthA=sqrt((xA-xAend)^2+(yA-yAend)^2)
+#probability of chord being longer than triangle side
+probA=mean(lengthA>lengthSide) 
 
-
-# Plot
-p1 <- ggplot() +
+# Plot A
+pA <- ggplot() +
   ggforce::geom_circle(aes(x0 = 0, y0 = 0, r = 1), col = "gray50") +
   geom_segment(data = eqtri_df, aes(x = x, y = y, xend = xend, yend = yend)) +
   geom_segment(data = rdmchr_A, aes(x = x, y = y, xend = xend, yend = yend),
                col = "red3") +
   coord_equal()
 
-ggsave(p1, file = "plotA.png", height = 5, width = 7)
+ggsave(pA, file = "plotA.png", height = 5, width = 7)
 
 # Method B............................................................................
-thetaB=2*pi*runif(numbLines); #choose angular component uniformly
-pB=r*runif(numbLines); #choose radial component uniformly
-qB=sqrt(r^2-pB^2); #distance to circle edge (along line)
+
+thetaB=2*pi*runif(numbLines) #choose angular component uniformly
+pB=r*runif(numbLines) #choose radial component uniformly
+qB=sqrt(r^2-pB^2) #distance to circle edge (along line)
 
 #calculate trig values
-sin_thetaB=sin(thetaB);
-cos_thetaB=cos(thetaB);
+sin_thetaB=sin(thetaB)
+cos_thetaB=cos(thetaB)
+
 # Coordinates of random chord
-#calculate segment endpoints
 
 rdmchr_B <- tibble(
   x    = x0+pB*cos_thetaB+qB*sin_thetaB,
@@ -57,14 +73,55 @@ rdmchr_B <- tibble(
 )
 
 # calculate probability that the chord is longer than a side of the triangle
-
-# Plot
-p2 <- ggplot() +
+xB    = x0+pB*cos_thetaB+qB*sin_thetaB
+yB    = y0+pB*sin_thetaB-qB*cos_thetaB
+xBend = x0+pB*cos_thetaB-qB*sin_thetaB
+yBend = y0+pB*sin_thetaB+qB*cos_thetaB
+lengthSide<-r*sqrt(3)
+lengthB=sqrt((xB-xBend)^2+(yB-yBend)^2)
+probB=mean(lengthB>lengthSide)
+# Plot B
+pB <- ggplot() +
   ggforce::geom_circle(aes(x0 = 0, y0 = 0, r = 1), col = "gray50") +
   geom_segment(data = eqtri_df, aes(x = x, y = y, xend = xend, yend = yend)) +
   geom_segment(data = rdmchr_B, aes(x = x, y = y, xend = xend, yend = yend),
                col = "red3") +
   coord_equal()
 
-ggsave(p2, file = "plotB.png", height = 5, width = 7)
+ggsave(pB, file = "plotB.png", height = 5, width = 7)
 
+#Method C.............................................................................
+
+thetaC=2*pi*runif(numbLines) #choose angular component uniformly
+pC=r*sqrt(runif(numbLines)) #choose radial component
+qC=sqrt(r^2-pC^2) #distance to circle edge (along line)
+
+#calculate trig values
+sin_thetaC=sin(thetaC)
+cos_thetaC=cos(thetaC)
+# Coordinates of random chord
+
+rdmchr_C <- tibble(
+  x    = x0+pC*cos_thetaC+qC*sin_thetaC,
+  y    = y0+pC*sin_thetaC-qC*cos_thetaC,
+  xend = x0+pC*cos_thetaC-qC*sin_thetaC,
+  yend = y0+pC*sin_thetaC+qC*cos_thetaC
+)
+# calculate probability that the chord is longer than a side of the triangle
+xC    = x0+pC*cos_thetaC+qC*sin_thetaC
+yC    = y0+pC*sin_thetaC-qC*cos_thetaC
+xCend = x0+pC*cos_thetaC-qC*sin_thetaC
+yCend = y0+pC*sin_thetaC+qC*cos_thetaC
+lengthSide<-r*sqrt(3)
+lengthC=sqrt((xC-xCend)^2+(yC-yCend)^2)
+probC=mean(lengthC>lengthSide)
+
+# Plot C
+pC <- ggplot() +
+  ggforce::geom_circle(aes(x0 = 0, y0 = 0, r = 1), col = "gray50") +
+  geom_segment(data = eqtri_df, aes(x = x, y = y, xend = xend, yend = yend)) +
+  geom_segment(data = rdmchr_B, aes(x = x, y = y, xend = xend, yend = yend),
+               col = "red3") +
+  coord_equal()
+
+ggsave(pC, file = "plotC.png", height = 5, width = 7)
