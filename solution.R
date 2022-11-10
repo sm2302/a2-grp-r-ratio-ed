e2fdf4bad2f794acf8c41b179367b45ccd31cc98
Mwalim19 <111836932+Mwalim19@users.noreply.github.com>
2022-11-10 17:53:09 +0800

    solution B

--- solution.R
+++ solution.R
@@ -3,10 +3,10 @@ library(ggforce)
 theme_set(theme_void()) 
 
 x0=0; y0=0; #center of disk
-r=1; #disk radius
-numbLines=100;#number of lines
+r=1; #radius
+numbLines=100; #number of lines
 
-# Method 1..........................................
+# Method A..........................................
 thetaA1=2*pi*runif(numbLines); #choose angular component uniformly
 thetaA2=2*pi*runif(numbLines); #choose angular component uniformly
 
@@ -19,7 +19,7 @@ eqtri_df <- tibble(
 )
 
 # Coordinates of random chord
-rdmchr_df <- tibble(
+rdmchr_A <- tibble(
   x    = x0+r*cos(thetaA1),
   y    = y0+r*sin(thetaA1),
   xend = x0+r*cos(thetaA2),
@@ -29,11 +29,42 @@ rdmchr_df <- tibble(
 
 
 # Plot
-p <- ggplot() +
+p1 <- ggplot() +
   ggforce::geom_circle(aes(x0 = 0, y0 = 0, r = 1), col = "gray50") +
   geom_segment(data = eqtri_df, aes(x = x, y = y, xend = xend, yend = yend)) +
-  geom_segment(data = rdmchr_df, aes(x = x, y = y, xend = xend, yend = yend),
+  geom_segment(data = rdmchr_A, aes(x = x, y = y, xend = xend, yend = yend),
                col = "red3") +
   coord_equal()
 
-ggsave(p, file = "plot.png", height = 5, width = 7)
+ggsave(p1, file = "plotA.png", height = 5, width = 7)
+
+# Method B............................................................................
+thetaB=2*pi*runif(numbLines); #choose angular component uniformly
+pB=r*runif(numbLines); #choose radial component uniformly
+qB=sqrt(r^2-pB^2); #distance to circle edge (along line)
+
+#calculate trig values
+sin_thetaB=sin(thetaB);
+cos_thetaB=cos(thetaB);
+# Coordinates of random chord
+#calculate segment endpoints
+
+rdmchr_B <- tibble(
+  x    = x0+pB*cos_thetaB+qB*sin_thetaB,
+  y    = y0+pB*sin_thetaB-qB*cos_thetaB,
+  xend = x0+pB*cos_thetaB-qB*sin_thetaB,
+  yend = y0+pB*sin_thetaB+qB*cos_thetaB
+)
+
+# calculate probability that the chord is longer than a side of the triangle
+
+# Plot
+p2 <- ggplot() +
+  ggforce::geom_circle(aes(x0 = 0, y0 = 0, r = 1), col = "gray50") +
+  geom_segment(data = eqtri_df, aes(x = x, y = y, xend = xend, yend = yend)) +
+  geom_segment(data = rdmchr_B, aes(x = x, y = y, xend = xend, yend = yend),
+               col = "red3") +
+  coord_equal()
+
+ggsave(p2, file = "plotB.png", height = 5, width = 7)
+
